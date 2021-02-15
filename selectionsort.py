d533dc64197e3856f2125d25fe5618c77b7de039
ehkater <73807717+ehkater@users.noreply.github.com>
2021-02-15 22:13:10 +1300

    read selection sort csv

--- selectionsort.py
+++ selectionsort.py
@@ -11,3 +11,13 @@ def selection_sort(list_a):
         if min_value != i:
             list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
     return list_a
+
+def read():
+    #read from file
+    f = open("data.csv","r")
+    data = f.read().split()# convert string to list by split
+    f.close()
+    # for each element in list convert to int
+    for i in range(len(data)):
+        data[i] = str(data[i])
+    return data
