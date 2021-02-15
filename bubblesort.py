f6a40966cfc31cf30de08f2354e217a67179212a
ehkater <73807717+ehkater@users.noreply.github.com>
2021-02-15 21:52:08 +1300

    read csv file and bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -4,3 +4,13 @@ def bubblesort(mylist):
             if mylist[j] > mylist[j+1]:
                 mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
     return mylist
+
+def read():
+    #read from file
+    f = open("data.csv","r")
+    data = f.read().split()# convert string to list by split
+    f.close()
+    # for each element in list convert to int
+    for i in range(len(data)):
+        data[i] = str(data[i])
+    return data
