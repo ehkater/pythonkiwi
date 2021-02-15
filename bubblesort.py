eb22406710ad8d2e3750a1b27aaa2918fc43e2ed
ehkater <73807717+ehkater@users.noreply.github.com>
2021-02-15 21:44:27 +1300

    bubble sort array

---
+++ bubblesort.py
@@ -0,0 +1,6 @@
+def bubblesort(mylist):
+    for i in range (0, len(mylist) - 1): 
+        for j in range (0 , len(mylist) - 1 - i):
+            if mylist[j] > mylist[j+1]:
+                mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+    return mylist
